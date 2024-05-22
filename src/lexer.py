b16801bb7103a1a45dc591d0eaf2edb0c015f7ae
sogekng <gutsantos3@gmail.com>
2024-05-22 15:00:26 -0300

    Modificando o arquivo lexer e o arquivo main

--- src/lexer.py
+++ src/lexer.py
@@ -10,19 +10,25 @@ class Lexer:
         self.current_line = 1
         self.current_column = 0
         self.text = ""
-
         self.token_specs = [
             ('STRING', r'\"([^\\\n]|(\\.))*?\"'),
             ('NUMBER', r'\d+'),
-            ('PLUS', r'\+'),
-            ('MINUS', r'-'),
-            ('TIMES', r'\*'),
-            ('DIVIDE', r'/'),
+            ('COMMENT', r'//.*'),
+            ('EQ', r'=='),
+            ('NEQ', r'!='),
+            ('GT', r'>'),
+            ('LT', r'<'),
+            ('GTE', r'>='),
+            ('LTE', r'<='),
+            ('ADDITION', r'\+'),
+            ('SUBTRACTION', r'-'),
+            ('MULTIPLICATION', r'\*'),
+            ('DIVISION', r'/'),
+            ('ASSIGNMENT', r'='),
             ('LPAREN', r'\('),
             ('RPAREN', r'\)'),
             ('LBRACE', r'\{'),
             ('RBRACE', r'\}'),
-            ('EQUALS', r'='),
             ('SEMICOLON', r';'),
             ('NEWLINE', r'\n'),
             ('SKIP', r'[ \t]+'),
@@ -30,8 +36,8 @@ class Lexer:
             ('IF', r'if'),
             ('ELSE', r'else'),
             ('WHILE', r'while'),
-            ('FOR', r'for'),
-            ('IDENTIFIER', r'[a-zA-Z_][a-zA-Z_0-9]*'),
+            ('DO', r'do'),
+            ('IDENTIFIER', r'[a-zA-Z_][a-zA-Z_0-9]*')
         ]
 
 
@@ -51,6 +57,39 @@ class Lexer:
         return self.tokens
 
 
+    def next_token(self):
+        while self.current_char is not None:
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char == '\n':
+                self.advance()
+            else:
+                for pattern, regex in self.token_specs:
+                    regex = re.compile(regex)
+                    match = regex.match(self.text, self.current_position)
+                    if match:
+                        lexeme = match.group(0)
+                        if pattern == 'COMMENT':
+                            while self.current_char != '\n' and self.current_char is not None:
+                                self.advance()
+                            break
+                        value = lexeme
+                        if pattern == 'NUMBER':
+                            value = int(value)
+                        elif pattern == 'STRING':
+                            value = value[1:-1]
+
+                        start_line = self.current_line
+                        start_column = self.current_column - len(lexeme)
+                        token = Token(pattern, value, start_line, start_column)
+                        self.current_position += len(lexeme) - 1
+                        self.advance()
+                        return token
+                else:
+                    self.advance()
+        return None
+
+
     def advance(self):
         self.current_position += 1
         if self.current_position >= len(self.text):
@@ -64,48 +103,42 @@ class Lexer:
                 self.current_column += 1
 
 
-    def next_token(self):
-        for pattern, regex in self.token_specs:
-            regex = re.compile(regex)
-            match = regex.match(self.text, self.current_position)
-            if match:
-                lexeme = match.group(0)
-                value = lexeme
-                if pattern == 'NUMBER':
-                    value = int(value)
-                elif pattern == 'STRING':
-                    value = value[1:-1]  # Remove as aspas
-
-                if pattern == 'NEWLINE':
-                    self.advance()
-                    return None
-                elif pattern == 'SKIP':
-                    self.advance()
-                    return None
-
-                token = Token(pattern, value, self.current_line, self.current_column)
-                self.current_position += len(lexeme) - 1
-                self.advance()
-                return token
-        self.advance()
-        return None
+    def log_token(self, token):
+        with open('tokens.log', 'a') as file:
+            file.write(f"{token}\n")
 
 
-if __name__ == "__main__":
-    lexer = Lexer()
-    data = '''
-        print("Hello, World!");
-        x = 10;
-        if (x > 5) {
-            x = x + 1;
-        } else {
-            x = x - 1;
-        }
-        while (x < 20) {
-            x = x + 2;
-        }
-        for(i=1;i<=10;i++){}
-    '''
-    tokens = lexer.tokenize(data)
-    for token in tokens:
-        print(token)
+# if __name__ == "__main__":
+#     lexer = Lexer()
+#     data = '''
+#         //print("Hello, World!");
+#
+#         //x = 10;
+#
+#         //if (x > 5) {
+#         //    x = x + 1;
+#         //} else {
+#         //    x = x - 1;
+#         //}
+#
+#         //while (x < 20) {
+#         //    x = x * 2;
+#         //}
+#
+#         //do {
+#         //    x = x / 3;
+#         //} while (x < 30);
+#
+#         // Teste de operadores relacionais
+#         y == 10;
+#         z != 5;
+#         a > 1;
+#         b < 2;
+#         c >= 3;
+#         d <= 4;
+#     '''
+#     tokens = lexer.tokenize(data)
+#     for token in tokens:
+#         print(token)
+#
+#     lexer.log_token(tokens)
---
+++ src/main.py
@@ -0,0 +1,20 @@
+from lexer import Lexer
+
+def main():
+    file_path = "teste"
+    try:
+        with open(file_path, 'r') as file:
+            code = file.read()
+            lexer = Lexer()
+            tokens = lexer.tokenize(code)
+
+            print("Tokens gerados:")
+            for token in tokens:
+                print(token)
+    except FileNotFoundError:
+        print(f"O arquivo {file_path} não foi encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao processar o arquivo: {e}")
+
+if __name__ == "__main__":
+    main()
